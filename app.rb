46ad14f024399f0afe8a042aaa9426432a8a3e0b
victoria633 <victoria6411@hotmail.com>
2018-06-12 21:35:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,34 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, Word!"
 end
+
+get '/name' do
+  "My name is Victoria"
+end 
+
+get '/hometown' do
+  "My hometown is Buckeye"
+end
+
+get '/favorite-song' do
+  "My favorite song is Life of the Party by Shawn Mendes"
+end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
